1184583e496082e04c0c098dccbf6b662bc60de5
Alok Subbarao <aloksubbarao@gmail.com>
2018-09-16 22:44:36 -0700

    working through the rosalind exercises

---
+++ bioinformatics_stronghold/counting_dna_nucleotides.py
@@ -0,0 +1,12 @@
+#http://rosalind.info/problems/dna/
+from collections import Counter 
+import pandas as pd
+
+
+def count_dna_sequence(path_to_textfile):
+
+  data = pd.read_csv(path_to_textfile)  #pandas thinks each character is a column and passes an empty dataframe
+  dna_string = data.columns[0] #access the string 
+  counted_bases = Counter(dna_string)
+
+  return counted_bases
